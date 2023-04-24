003782d61a87d46ae44ba62839b5f240fe6249fb
gersonkaitor <gersonkaitor@gmail.com>
2023-04-24 16:45:02 +0800

    CRUD Rest Api

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Room = require('../models/roomModel');
-const User = require('../models/userModels');
 
 const users = [
   {
@@ -24,39 +23,97 @@ const users = [
 const rooms = [
   {
     id: 1,
-    user: [1],
+    userId: 1,
     room: 'Room 1',
     password: 'room123',
   },
   {
     id: 2,
     room: 'Room 2',
-    user: 2,
+    userId: 2,
     password: 'room123',
   },
   {
     id: 3,
-    user: 3,
-    room: 'Room 1',
+    userId: 3,
+    room: 'Room 3',
     password: 'room123',
   },
 ];
 
+// @desc Get a room
+// @route GET /api/room
+// @access Private
 router.get('/', (req, res) => {
-  const room = rooms.find((room) => room.id === +req.params.roomId);
+  const room = rooms.find((room) => room.room === req.body.room);
 
   if (!room) {
-    res.status(404).json({ success: false, error: 'Resource not found' });
+    res.status(404).json({ success: false, error: 'Room not found' });
   }
 
-  const roomsWithUser = rooms.map((room) => {
-    if (room.user === users.id) {
-      return res.json({ success: true, data: { rooms, user: users.username } });
-    }
-  });
+  res.json({ success: true, data: room });
+});
 
-  // res.json({ success: true, data: { rooms, users } });
-  res.json({ roomsWithUser });
+// @desc Create a room
+// @route POST /api/room
+// @access Private
+router.post('/', (req, res) => {
+  let length = 30,
+    charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+    randomPassword = '';
+  for (let i = 0, n = charset.length; i < length; ++i) {
+    randomPassword += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  const createRoom = {
+    id: rooms.length + 1,
+    room: req.body.room,
+    password: randomPassword || req.body.password,
+    date: new Date().toISOString().slice(0, 10),
+  };
+
+  // Check data
+  if (!createRoom.room || !createRoom.password) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'All fields are required' });
+  }
+  rooms.push(createRoom);
+
+  const room = rooms.find((room) => createRoom.id === room.id);
+  res.json({ success: true, data: room });
+});
+
+// @desc Update a room
+// @route PUT /api/room
+// @access Private
+router.put('/:id', (req, res) => {
+  const room = rooms.find((room) => room.id === +req.params.id);
+
+  if (!room) {
+    res.status(404).json({ success: false, error: 'Room not found' });
+  }
+
+  room.room = req.body.room || room.room;
+  room.password = req.body.password || room.password;
+
+  res.json({ success: true, data: room });
+});
+
+// @desc Delete a room
+// @route DELETE /api/room
+// @access Private
+router.delete('/:id', (req, res) => {
+  const room = rooms.find((room) => room.id === +req.params.id);
+
+  if (!room) {
+    res.status(404).json({ success: false, error: 'Room not found' });
+  }
+
+  const index = rooms.indexOf(room);
+  rooms.splice(index, 1);
+
+  res.json({ success: true, data: {} });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const PORT = 5000;
 
 const app = express();
 
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the BINGO API' });
 });
 
 const roomRouter = require('./routes/roomRoutes');
-app.use('/api/rooms', roomRouter);
+app.use('/api/room', roomRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
